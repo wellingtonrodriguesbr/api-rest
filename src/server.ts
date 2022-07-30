aa64d2a93a1ceb6c63008164f1dab67e661c93f4
Wellington Rodrigues <wr_rodrigues@outlook.com.br>
2022-07-30 12:17:58 -0300

    fix: fix route static

--- src/server.ts
+++ src/server.ts
@@ -5,6 +5,6 @@ import path from "path";
 const app = express();
 
 app.use(routes);
-app.use("uploads", express.static(path.resolve(__dirname, "..", "uploads")));
+app.use("/uploads", express.static(path.resolve(__dirname, "..", "uploads")));
 
 app.listen(3333, () => console.log("Server is running!"));
