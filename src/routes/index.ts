70402f87e6caae55f23548cd6e1b24038e4f0b35
Wellington Rodrigues <wr_rodrigues@outlook.com.br>
2022-07-30 12:46:04 -0300

    feat: create route items list

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,9 +1,8 @@
 import { Router } from "express";
+import itemsRouter from "./items.routes";
 
 const routes = Router();
 
-routes.get("/", (req, res) => {
-  return res.json({ message: "Hello World with NodeJs and TypeScript!!!" });
-});
+routes.use("/items", itemsRouter);
 
 export default routes;
