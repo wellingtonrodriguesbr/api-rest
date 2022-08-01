24a414a9159382e98c31a71648349ed21e9fcf65
Wellington Rodrigues <wr_rodrigues@outlook.com.br>
2022-08-01 06:59:08 -0300

    feat: added celebrate for data validation

--- src/routes/locations.routes.ts
+++ src/routes/locations.routes.ts
@@ -1,13 +1,34 @@
 import { Router } from "express";
-import LocationsController from "../controllers/LocationsController";
 import { upload } from "../controllers/LocationsController";
+import LocationsController from "../controllers/LocationsController";
+import { celebrate, Joi } from "celebrate";
 
 const locationsRouter = Router();
 const locationsController = new LocationsController();
 
 locationsRouter.get("/", locationsController.index);
 locationsRouter.get("/:id", locationsController.show);
-locationsRouter.post("/", locationsController.create);
+locationsRouter.post(
+  "/",
+  celebrate(
+    {
+      body: Joi.object().keys({
+        name: Joi.string().required(),
+        email: Joi.string().required().email(),
+        whatsapp: Joi.string().required(),
+        latitude: Joi.number().required(),
+        longitude: Joi.number().required(),
+        city: Joi.string().required(),
+        uf: Joi.string().required().max(2),
+        items: Joi.array().items(Joi.number()).required(),
+      }),
+    },
+    {
+      abortEarly: false,
+    }
+  ),
+  locationsController.create
+);
 locationsRouter.put("/:id", upload.single("image"), locationsController.update);
 
 export default locationsRouter;
